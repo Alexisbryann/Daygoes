b59d6f2ad09f0ca997f795cb4001ef993e2b4dc6
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-12-30 18:48:57 +0300

    individual matatu slider moved in from main activity

--- app/src/main/java/com/alexis/matatu/Adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/alexis/matatu/Adapters/ViewPagerAdapter.java
@@ -1,4 +1,60 @@
 package com.alexis.matatu.Adapters;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.alexis.matatu.RoutesFragment;
+import com.alexis.matatu.VehicleFragment;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        if (position == 0) {
+            fragment = new VehicleFragment();
+        }else if (position == 1){
+            fragment = new RoutesFragment();
+        }else if (position == 2){
+//            fragment = new BlackFridayDealsFragment();
+        }else if (position == 3){
+            fragment = new Fragment();
+        }
+        assert fragment != null;
+        return fragment;    }
+
+
+        @Nullable
+        @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if (position==0){
+            title = "View vehicles";
+        }
+        else if (position==1) {
+            title = "Routes";
+        }
+        else if (position==2){
+            title = "Black Friday";
+        }
+        else if (position==3){
+            title = "Help";
+        }
+        return title;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/alexis/matatu/RoutesFragment.java
@@ -0,0 +1,6 @@
+package com.alexis.matatu;
+
+import androidx.fragment.app.Fragment;
+
+public class RoutesFragment extends Fragment {
+}
