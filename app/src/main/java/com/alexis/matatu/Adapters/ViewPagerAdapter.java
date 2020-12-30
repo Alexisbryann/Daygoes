9355de595b5f6110301392b4b34177a279f02467
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-12-30 14:33:42 +0300

    individual matatu layout

---
+++ app/src/main/java/com/alexis/matatu/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.alexis.matatu.Adapters;
+
+public class ViewPagerAdapter {
+}
---
+++ app/src/main/java/com/alexis/matatu/IndividualMatatu.java
@@ -0,0 +1,14 @@
+package com.alexis.matatu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IndividualMatatu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.individual_matatu);
+    }
+}
