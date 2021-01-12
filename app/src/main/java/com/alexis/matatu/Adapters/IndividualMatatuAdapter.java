faed5f65896c581ef4d1bfc91c3ea4d146158e1c
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2021-01-12 17:07:48 +0300

    Working on individual matatu adapter.

--- app/src/main/java/com/alexis/matatu/Adapters/IndividualMatatuAdapter.java
+++ app/src/main/java/com/alexis/matatu/Adapters/IndividualMatatuAdapter.java
@@ -2,13 +2,19 @@ package com.alexis.matatu.Adapters;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alexis.matatu.Models.IndividualMatatuModel;
+import com.alexis.matatu.R;
+import com.daimajia.slider.library.SliderLayout;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.button.MaterialButton;
 
 public class IndividualMatatuAdapter extends FirebaseRecyclerAdapter<IndividualMatatuModel,IndividualMatatuAdapter.ViewHolder> {
 
@@ -30,6 +36,17 @@ public class IndividualMatatuAdapter extends FirebaseRecyclerAdapter<IndividualM
     public class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            TextView tv_goes = itemView.findViewById(R.id.tv_goes);
+            TextView tv_name = itemView.findViewById(R.id.tv_matatu_name);
+            TextView tv_plate = itemView.findViewById(R.id.tv_plate);
+            TextView tv_route = itemView.findViewById(R.id.tv_route);
+            SliderLayout slider = itemView.findViewById(R.id.slider);
+            ImageView like = itemView.findViewById(R.id.img_like);
+            ImageView favourite = itemView.findViewById(R.id.img_favourite);
+            ImageView share = itemView.findViewById(R.id.img_share);
+            ImageView dislike = itemView.findViewById(R.id.img_dislike);
+            MaterialButton pay = itemView.findViewById(R.id.btn_payWithPoints);
+            RatingBar ratingBar = itemView.findViewById(R.id.ratingBar);
         }
     }
 }
