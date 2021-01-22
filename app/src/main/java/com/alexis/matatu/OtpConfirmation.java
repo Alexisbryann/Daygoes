bbd54ad559350338ecefbaec4620e336e282f681
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2021-01-22 12:47:37 +0300

    OTP login layout done

---
+++ app/src/main/java/com/alexis/matatu/OtpConfirmation.java
@@ -0,0 +1,14 @@
+package com.alexis.matatu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtpConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_confirmation);
+    }
+}
