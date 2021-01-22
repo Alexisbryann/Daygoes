0e37c1280a08bee408098cbc06c873df2b8ace8e
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2021-01-22 17:31:47 +0300

    OTP verification working

--- app/src/main/java/com/alexis/matatu/Login.java
+++ app/src/main/java/com/alexis/matatu/Login.java
@@ -1,6 +1,138 @@
 package com.alexis.matatu;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private Button mSendOTP;
+    private ProgressBar mProgressBar;
+    private MaterialButton mResendOTP;
+    private TextInputEditText mPhoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        mPhoneNumber = findViewById(R.id.enter_phone_no);
+        mSendOTP = findViewById(R.id.button_send_otp);
+        mProgressBar = findViewById(R.id.progressBar);
+        mResendOTP = findViewById(R.id.btn_resend_otp);
+
+        mSendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                String countryExtension = String.valueOf(+254);
+                String phoneNumber = mPhoneNumber.getText().toString();
+//                String completeNumber = countryExtension + phoneNumber;
+
+                if ( phoneNumber.isEmpty()){
+                    mPhoneNumber.setError("Phone number can not be empty");
+                }else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mSendOTP.setEnabled(false);
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phoneNumber,        // Phone number to verify
+                            60,                 // Timeout duration
+                            TimeUnit.SECONDS,   // Unit of timeout
+                            Login.this,               // Activity (for callback binding)
+                            mCallbacks);        // OnVerificationStateChangedCallbacks
+                }
+            }
+        });
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+                mSendOTP.setVisibility(View.GONE);
+                mPhoneNumber.setError("OTP sending failed, please resend");
+                mResendOTP.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onCodeSent(final String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+//                new android.os.Handler().postDelayed(
+//                        new Runnable() {
+//                            public void run() {
+                Intent otpIntent = new Intent(Login.this, OtpConfirmation.class);
+                otpIntent.putExtra("AuthCredentials", s);
+                startActivity(otpIntent);
+            }
+//                        },
+//                        10000);
+//            }
+        };
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mCurrentUser != null){
+            sendUserToHome();
+        }
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            sendUserToHome();
+                            // Sign in success, update UI with the signed-in user's information
+                            // ...
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                mPhoneNumber.setError("OTP sending failed, please resend");
+                            }
+                        }
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+    }
+    public void sendUserToHome(){
+        Intent homeIntent = new Intent(Login.this, MainActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(homeIntent);
+        finish();
+    }
 }
