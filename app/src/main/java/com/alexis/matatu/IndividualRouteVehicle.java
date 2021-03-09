a6b6e37e26cd06b4ac65ab9a60d2387de036b999
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2021-03-09 13:46:00 +0300

    Unfavourite working

--- app/src/main/java/com/alexis/matatu/IndividualRouteVehicle.java
+++ app/src/main/java/com/alexis/matatu/IndividualRouteVehicle.java
@@ -398,11 +398,15 @@ public class IndividualRouteVehicle extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    Toast.makeText(IndividualRouteVehicle.this, "Already added " + mName + " to your favourites", Toast.LENGTH_LONG).show();
+                    dataSnapshot.getRef().removeValue();
+                    mFavourite.setColorFilter(Color.rgb(221,221,221), PorterDuff.Mode.SRC_IN);
+                    Toast.makeText(IndividualRouteVehicle.this, "Removed " + mName + " from your favourites", Toast.LENGTH_LONG).show();
 
                 } else {
                     favourites.setValue(mUserId);
                     mFavourite.setColorFilter(Color.rgb(255, 191, 0), PorterDuff.Mode.SRC_IN);
+                    Toast.makeText(IndividualRouteVehicle.this, "Added " + mName + " to your favourites", Toast.LENGTH_LONG).show();
+
                 }
             }
 
