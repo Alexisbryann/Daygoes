7db08d546a0c9939c35c058e26c4507c774fa1d5
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2021-09-21 22:08:58 +0300

    VideosFragment now working

--- app/src/main/java/com/alexis/daygoes/Fragments/VideosFragment.java
+++ app/src/main/java/com/alexis/daygoes/Fragments/VideosFragment.java
@@ -1,16 +1,14 @@
 package com.alexis.daygoes.Fragments;
 
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
@@ -20,150 +18,57 @@ import androidx.fragment.app.Fragment;
 import com.alexis.daygoes.R;
 
 public class VideosFragment extends Fragment {
+    private VideoView videoView;
+    private EditText uriEditText;
+    private Button playButton;
 
-    private static final String VIDEO_SAMPLE =
-            "gs://simple-shopping-290e2.appspot.com/ngav.mp4";
-
-    private VideoView mVideoView;
-    private TextView mBufferingTextView;
-
-    // Current playback position (in milliseconds).  
-    private int mCurrentPosition = 0;
-
-    // Tag for the instance state bundle.  
-    private static final String PLAYBACK_TIME = "play_time";
-
-    public VideosFragment() {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-
-    private View mView;
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_videos, container, false);
 
-        mView = inflater.inflate(R.layout.activity_videos, container, false);
+        videoView = view.findViewById(R.id.fragment_webvideo_videoView);
+        playButton = view.findViewById(R.id.fragment_webvideo_play);
 
-        mVideoView = mView.findViewById(R.id.videoview);
-        mBufferingTextView = mView.findViewById(R.id.buffering_textview);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playVideoFromWeb();
+            }
+        });
 
-        if (savedInstanceState != null) {
-            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
-        }
-
-        // Set up the media controller widget and attach it to the video view.  
-        MediaController controller = new MediaController(getContext());
-        controller.setMediaPlayer(mVideoView);
-        mVideoView.setMediaController(controller);
-        return null;
+        return view;
     }
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // Load the media each time onStart() is called.  
-        initializePlayer();
+    private void playVideoFromWeb() {
+        Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/simple-shopping-290e2.appspot.com/o/ngav.mp4?alt=media&token=13e6858a-7993-4cbc-a341-8ba4b0042d2d");
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
     }
+
 
     @Override
     public void onPause() {
         super.onPause();
 
-        // In Android versions less than N (7.0, API 24), onPause() is the  
-        // end of the visual lifecycle of the app.  Pausing the video here  
-        // prevents the sound from continuing to play even after the app  
-        // disappears.  
-        //  
-        // This is not a problem for more recent versions of Android because  
-        // onStop() is now the end of the visual lifecycle, and that is where  
-        // most of the app teardown should take place.  
+        // In Android versions less than N (7.0, API 24), onPause() is the
+        // end of the visual lifecycle of the app.  Pausing the video here
+        // prevents the sound from continuing to play even after the app
+        // disappears.
+        //
+        // This is not a problem for more recent versions of Android because
+        // onStop() is now the end of the visual lifecycle, and that is where
+        // most of the app teardown should take place.
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            mVideoView.pause();
+            videoView.pause();
         }
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // Media playback takes a lot of resources, so everything should be  
-        // stopped and released at this time.  
-        releasePlayer();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        // Save the current playback position (in milliseconds) to the  
-        // instance state bundle.  
-        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
-    }
-
-    private void initializePlayer() {
-        // Show the "Buffering..." message while the video loads.  
-        mBufferingTextView.setVisibility(VideoView.VISIBLE);
-
-        // Buffer and decode the video sample.  
-        Uri videoUri = getMedia(VIDEO_SAMPLE);
-        mVideoView.setVideoURI(videoUri);
-
-        // Listener for onPrepared() event (runs after the media is prepared).  
-        mVideoView.setOnPreparedListener(
-                new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mediaPlayer) {
-
-                        // Hide buffering message.  
-                        mBufferingTextView.setVisibility(VideoView.INVISIBLE);
-
-                        // Restore saved position, if available.  
-                        if (mCurrentPosition > 0) {
-                            mVideoView.seekTo(mCurrentPosition);
-                        } else {
-                            // Skipping to 1 shows the first frame of the video.  
-                            mVideoView.seekTo(1);
-                        }
-
-                        // Start playing!  
-                        mVideoView.start();
-                    }
-                });
-
-        // Listener for onCompletion() event (runs after media has finished  
-        // playing).  
-        mVideoView.setOnCompletionListener(
-                new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Toast.makeText(getContext(),
-                                "Video completed",
-                                Toast.LENGTH_SHORT).show();
-
-                        // Return the video position to the start.  
-                        mVideoView.seekTo(0);
-                    }
-                });
-    }
-
-
-    // Release all media-related resources. In a more complicated app this  
-    // might involve unregistering listeners or releasing audio focus.  
-    private void releasePlayer() {
-        mVideoView.stopPlayback();
-    }
-
-    // Get a Uri for the media sample regardless of whether that sample is  
-    // embedded in the app resources or available on the internet.  
-    private Uri getMedia(String mediaName) {
-        if (URLUtil.isValidUrl(mediaName)) {
-            // Media name is an external URL.  
-            return Uri.parse(mediaName);
-        }
-        return null;
-    }
-
-}  
+}
